c5b1de63981015b6307eb74e4d0c8ad1e01d71c8
urielbrunomota <ubumo@hotmail.com>
2025-06-10 15:51:46 -0400

    added maze.py and tested

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
 from graphics import Window, Line, Point
 from cell import Cell
+from maze import Maze
+
 
 def main():
-    win = Window(800, 600)  # You can change the width and height as needed
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)  # You can change the width and height as needed
+    
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+
     #l = Line(Point(50, 50), Point(400, 400))
     #win.draw_line(l, "black")
 
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(50, 50, 100, 100)
+#    c = Cell(win)
+ #   c.has_left_wall = False
+  #  c.draw(50, 50, 100, 100)
 
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+#    c = Cell(win)
+#    c.has_right_wall = False
+#    c.draw(125, 125, 200, 200)
 
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(225, 225, 250, 250)
+#    c = Cell(win)
+#    c.has_bottom_wall = False
+#    c.draw(225, 225, 250, 250)
 
-    c = Cell(win)
-    c.has_top_wall = False
-    c.draw(300, 300, 500, 500)
+#    c = Cell(win)
+#    c.has_top_wall = False
+#    c.draw(300, 300, 500, 500)
 
+#    c1 = Cell(win)
+#    c1.has_right_wall = False
+#    c1.draw(50, 50, 100, 100)
+
+#    c2 = Cell(win)
+#    c2.has_left_wall = False
+#    c2.has_bottom_wall = False
+#    c2.draw(100, 50, 150, 100)
+
+#    c1.draw_move(c2)
+
+#    c3 = Cell(win)
+#    c3.has_top_wall = False
+#    c3.has_right_wall = False
+#    c3.draw(100, 100, 150, 150)
+
+#    c2.draw_move(c3)
+
+#    c4 = Cell(win)
+#    c4.has_left_wall = False
+#    c4.draw(150, 100, 200, 150)
+
+#    c3.draw_move(c4, True)
 
     win.wait_for_close()
 
---
+++ maze.py
@@ -0,0 +1,42 @@
+from cell import Cell
+import random
+import time
+
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.__cells = []
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
+
+        self.__create_cells()
+
+    def __create_cells(self):
+        for i in range(self.__num_cols):
+            col_cells = []
+            for j in range(self.__num_rows):
+                col_cells.append(Cell(self.__win))
+            self.__cells.append(col_cells)
+        for i in range(self.__num_cols):
+            for j in range(self.__num_rows):
+                self.__draw_cell(i, j)
+
+    def __draw_cell(self, i, j):
+        if self.__win is None:
+            return
+        x1 = self.__x1 + i * self.__cell_size_x
+        y1 = self.__y1 + j * self.__cell_size_y
+        x2 = x1 + self.__cell_size_x
+        y2 = y1 + self.__cell_size_y
+        self.__cells[i][j].draw(x1, y1, x2, y2)
+        self.__animate()
+
+    def __animate(self):
+        if self.__win is None:
+            return
+        self.__win.redraw()
+        time.sleep(0.05)
